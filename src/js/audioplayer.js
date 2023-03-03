4fd398bd268a54dd269ce618ab1c343fc2ddbfab
IvanLomovtsev <i.d.lomovtsev@gmail.com>
2023-03-03 21:41:19 +0500

    feat: add opportunity to create play-list on page

--- src/js/audioplayer.js
+++ src/js/audioplayer.js
@@ -6,20 +6,32 @@ const prevTrack = document.querySelector('.play-prev');
 let isPlay = false;
 let numTrack = 0;
 const audio = new Audio();
+const playListContainer = document.querySelector('.play-list');
 
+//create play-list on page
+playList.forEach(el => {
+    const li = document.createElement('li');
+    li.classList.add('play-item');
+    playListContainer.append(li)
+    li.textContent = el.title;
+  })
+
+//play or pause a track
 function playAudio() {
   audio.src = playList[numTrack].src;
   audio.currentTime = 0;
-  console.log(isPlay)
   !isPlay ? audio.pause() : audio.play();
 }
 
+//switch to the next track
 function nextAudio(){
     numTrack < playList.length-1 ? numTrack++ : numTrack = 0;
     isPlay = true;
     playTrack.classList.add('pause');
     playAudio()
 }
+
+//switch to the next track
 function prevAudio(){
     numTrack === 0 ? numTrack = playList.length-1 : numTrack--;
     isPlay = true;
